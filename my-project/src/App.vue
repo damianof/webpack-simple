bba7e20626fa57987f6f649f3e06b570427fb179
Damiano <damiano@iMacRetina.fios-router.home>
2018-09-24 12:26:09 -0400

    LAtest

---
+++ my-project/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1></h1>
+    
+  </div>
+</template>
+<script src="./App.js"></script>
+<style src="./App.css"></style>
---
+++ my-project/src/Report.vue
@@ -0,0 +1,8 @@
+<template>
+  [report TODO]
+	<div id="report">
+		[<h1 v-text="name"></h1>]
+	</div>
+</template>
+<script src="./Report.js"></script>
+<style src="./Report.css"></style>
